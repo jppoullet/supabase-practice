4b6533cebbafe41d6edcc7e0af08571a758ee04e
jppoullet <jppoullet@yahoo.com>
2024-12-17 19:26:30 -0500

    login and theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { createClient } from "@supabase/supabase-js";
 import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 import Countries from "./components/countries";
-
-const supabase = createClient(
-  "https://hkbgripquxrybkogguai.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrYmdyaXBxdXhyeWJrb2dndWFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5NjgzMzYsImV4cCI6MjA0OTU0NDMzNn0.S_3i97n-HgznZLdyhxeZtBeZl5xPwpVLDYGBmXKxvJU"
-);
+import supabase from "./config/supabaseClient";
+import Test from "./components/Test";
 
 function App() {
   const [session, setSession] = useState(null);
@@ -26,10 +23,17 @@ function App() {
     return () => subscription.unsubscribe();
   }, []);
 
+  // const user = supabase.auth.getUser();
+  const {
+    data: { user },
+  } = supabase.auth.getUser();
+
+  console.log(user);
+
   if (!session) {
-    return <Auth supabaseClient={supabase} />;
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
   } else {
-    return <Countries></Countries>;
+    return <Countries />;
   }
 }
 
--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
+// import { createClient } from "@supabase/supabase-js";
+import supabase from "../config/supabaseClient";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
-
-  const supabase = createClient(
-    "https://hkbgripquxrybkogguai.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrYmdyaXBxdXhyeWJrb2dndWFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5NjgzMzYsImV4cCI6MjA0OTU0NDMzNn0.S_3i97n-HgznZLdyhxeZtBeZl5xPwpVLDYGBmXKxvJU"
-  );
 
   useEffect(() => {
     getCountries();
   }, []);
 
   const getCountries = async () => {
-    const { data } = await supabase.from("countries").select();
+    const { data } = await supabase.from("countries").select("*");
     setCountries(data);
+    console.log(data);
   };
 
   return (
-    <ul>
-      {countries.map((country) => (
-        <li key={country.name}>{country.name}</li>
-      ))}
-    </ul>
+    <div>
+      <div>Logged in! Countries</div>
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name}>{country.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
